52026a48d64793c03efa3feb7e8999a408b6326e
Matheus Rambo <matheusrambo24@gmail.com>
2024-08-02 19:42:29 -0300

    feat: C compatible strings #6

--- examples/basic_usage/simplestring.c
+++ examples/basic_usage/simplestring.c
@@ -6,11 +6,7 @@
 int main ( int argc, char **argv )
 {
     scs_t string = scs_from_string ( "Hello, my name is sgt-cortez!. Nice to meet with you ..." );
-
-    char *str = scs_to_string ( string );
-
-    puts ( str );
+    puts ( string );
     scs_free ( string );
-
     return 0;
 }
